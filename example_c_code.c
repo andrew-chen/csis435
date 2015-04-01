4e3720da7c8699b1430474abaccf1fe4e3403a9e
Andrew Chen <andrew@gmail.com>
2015-04-01 10:59:14 -0500

    Added some example c code for testing purposes

---
+++ example_c_code.c
@@ -0,0 +1,44 @@
+typedef struct foobar {
+    int f;
+    int b;
+    struct foobar * fb;
+} foobar;
+int q[100];
+foobar w[100];
+int z;
+int foo(int a, int b) {
+    int x;
+    int y;
+    return (x+y);
+};
+int bar(int c, int d) {
+    int y;
+    int z;
+};
+test() {};
+typedef int strange_unit;
+strange_unit bob;
+char a;
+unsigned char b;
+signed char c;
+short d;
+unsigned short e;
+signed short f;
+long g;
+unsigned long h;
+signed long i;
+typedef struct zoo {
+char a;
+unsigned char b;
+signed char c;
+short d;
+unsigned short e;
+signed short f;
+long g;
+unsigned long h;
+signed long i;
+} zoo;
+class Test {
+	int i;
+};
+
